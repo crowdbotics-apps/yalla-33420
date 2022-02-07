38c93406f1056ed85b97a4b7e4750aa6d75b7d47
Crowdbotics <team@crowdbotics.com>
2022-02-07 16:37:54 +0000

    Initial commit.

---
+++ backend/yalla_33420/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for yalla_33420 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yalla_33420.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "yalla_33420",
+  url: "https://yalla_33420.botics.co",
+  api: "https://yalla_33420.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
